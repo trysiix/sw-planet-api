c340132ba348187c6015e9afe54b151e602ca5d9
Try <trysiix@localhost.localdomain>
2020-07-15 23:17:52 -0300

    Creating Add function in middleware, this function generates new data records inside mongo db

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -2,8 +2,12 @@ package middleware
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+
+	"../models"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -48,4 +52,32 @@ func init() {
 
 	fmt.Println("Collection instance created!")
 
+}
+
+// Add creates the 'add' route to register the planet info, executes on call
+func Add(writer http.ResponseWriter, r *http.Request) {
+	writer.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
+	writer.Header().Set("Access-Control-Allow-Methods", "POST")
+	writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	var planet models.Planet
+	_ = json.NewDecoder(r.Body).Decode(&planet)
+
+	fmt.Println(planet, r.Body)
+
+	addPlanet(planet)
+
+	json.NewEncoder(writer).Encode(planet)
+}
+
+// Insert the planet data into mongo db
+func addPlanet(planet models.Planet) {
+	insertResult, err := collection.InsertOne(context.Background(), planet)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("New Planet Added", insertResult.InsertedID)
 }
